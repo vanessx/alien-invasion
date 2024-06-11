a45e45704feef64a78c46b89becab8784c2f856f
vanessa <51798590+vanessx@users.noreply.github.com>
2024-06-11 16:28:47 +0200

    Initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys 
+
+import pygame
+
+class AlienInvasion:
+    """classe geral para gerir o comportamento do jogo"""
+    def __init__(self):
+        """iniciar o jogo e criar os recursos"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('Alien Invasion')
+    
+    def run_game(self):
+        """começar o loop principal do jogo"""
+        while True:
+            # eventos do teclado e rato
+            for event in pygame.even.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # mostrar ecrã
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # criar uma instância do jogo e rodá-lo
+    ai = AlienInvasion()
+    ai.run_game()
