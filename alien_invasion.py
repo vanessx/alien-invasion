cd2ec85f7036f67ab8d7c57505b54e1616ed5a01
vanessa <51798590+vanessx@users.noreply.github.com>
2024-09-04 13:11:25 +0200

    commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,12 +2,17 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """classe geral para gerir o comportamento do jogo"""
+
     def __init__(self):
         """iniciar o jogo e criar os recursos"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
     
     def run_game(self):
@@ -17,6 +22,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    
+            # redesenhar o ecrã durante cada passagem pelo loop
+            self.screen.fill(self.settings.bg_color)
+
             # mostrar ecrã
             pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """ uma classe para guardar todas as definições do Alien Invasion"""
+    
+    def __init__(self):
+        """ inicializar as definições do jogo"""
+        
+        # definições do ecrã
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230) # definir uma cor de fundo em RGB
