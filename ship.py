c5349f33b7b00b6ec9e0be4dbbe764e1559da5c1
vanessa <51798590+vanessx@users.noreply.github.com>
2024-09-04 16:37:37 +0200

    commit

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """ inicializar a nave e definir a posição inicial """
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # carregar a imagem da nave e obter o seu retângulo
@@ -15,16 +16,23 @@ class Ship:
         # iniciar cada nova nave na parte inferior central do ecrã
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # guardar um valor decimal para a posição horizontal da nave
+        self.x = float(self.rect.x)
+
         # movimento da flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """ atualizar a posição da nave baseada no movimento da flag """
+        # atualizar o valor x da nave, não do rectângulo
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # atualizar o rectângulo pelo self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """ desenhar a nave na localização atual """
